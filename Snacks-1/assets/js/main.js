4d5d1e53c58de90c83d574bc8a45c7394989109c
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-28 10:59:13 +0200

    Snacks-1

---
+++ Snacks-1/assets/js/main.js
@@ -0,0 +1,12 @@
+// L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+var num1 = prompt("Inserisci il primo numero");
+var num2 = prompt("Inserisci il secondo numero");
+
+if(num1 > num2){
+    console.log(`Il numero maggiore è ${num1}`)
+} else if(num2 > num1){
+    console.log(`Il numero maggiore è ${num2}`)
+} else{
+    console.log(`I due numeri sono uguali`)
+}
